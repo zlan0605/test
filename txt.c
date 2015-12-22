cd166ec1bf61295e69baeba2526a983f0b2ab5fe
zlan <3080710953@qq.com>
2015-12-22 09:47:16 +0800

    first

--- txt.c
+++ txt.c
@@ -2,8 +2,6 @@
 int main()
 {
 printf("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-printf("bbbbbbbbbbbbbbbbbb");
-printf("ccccccccccccccccc");
 printf("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 printf("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 printf("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
